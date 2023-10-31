2feff21ddbdd83f21db3f5199ac4a5beb84f517a
Long <trandohoanglong113@gmail>
2023-10-31 17:46:36 +0700

    Tạo các action phía backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
 
 dotenv.config()
 app.use(express.json())
@@ -12,6 +15,10 @@ mongoose.connect("mongodb+srv://longtran:longtran123@cluster0.nexritu.mongodb.ne
 .catch((err) => console.log("err:" , err))
 
 app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+app.use("/api/categories", categoryRoute)
+
 
 app.listen("5000", () => {
     console.log("Backend is running");
