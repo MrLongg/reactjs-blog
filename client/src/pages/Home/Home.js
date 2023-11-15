bb75cf34ac62a940edfe8e8dc8fdcf936f129190
Long <trandohoanglong113@gmail>
2023-11-15 17:24:28 +0700

    Add pagination (còn lỗi css)

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -12,46 +12,56 @@ const cx = classNames.bind(styles);
 
 function Home() {
     const [posts, setPosts] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
     const { search } = useLocation();
+    const [currentPage, setCurrentPage] = useState(0);
+    const postsPerPage = 4;
 
-    const handlePageClick = (e) => {};
+    const handlePageClick = (e) => {
+        setCurrentPage(e.selected);
+    };
 
     useEffect(() => {
-        window.scrollTo(0, 0);
         const fetchPosts = async () => {
             const res = await axios.get('/posts' + search);
             setPosts(res.data);
+            setPageCount(Math.ceil(res.data.length / postsPerPage));
         };
+        window.scrollTo(0, 0);
         fetchPosts();
-    }, [search]);
+    }, [search, postsPerPage]);
+
+    const offset = currentPage * postsPerPage;
+    const currentPosts = posts.slice(offset, offset + postsPerPage);
 
     return (
         <>
             <Header />
             <div className={cx('wrapper')}>
-                <Posts posts={posts} />
+                <Posts posts={currentPosts} />
                 <Sidebar />
             </div>
-            <ReactPaginate
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={2}
-                pageCount={69}
-                previousLabel="< previous"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakLabel="..."
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
-                renderOnZeroPageCount={null}
-            />
+            
+                <ReactPaginate
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                />
         </>
     );
 }
