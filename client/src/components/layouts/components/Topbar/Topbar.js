a2dcc7698c6cfd7dabdcdaf5beefc23668b5cfc6
Long <trandohoanglong113@gmail>
2023-11-16 21:34:12 +0700

    Làm chức năng Search

--- client/src/components/layouts/components/Topbar/Topbar.js
+++ client/src/components/layouts/components/Topbar/Topbar.js
@@ -2,23 +2,72 @@ import classNames from 'classnames/bind';
 import styles from './Topbar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faPinterest, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Link, useLocation } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Context } from '~/context/Context';
+import axios from 'axios';
+import ToastMessage from '../ToastMessage';
 
 const cx = classNames.bind(styles);
 
 function Topbar() {
     const { user, dispatch } = useContext(Context);
-    const location = useLocation()
+    const [isInputActive, setIsInputActive] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const location = useLocation();
     const PF = 'http://127.0.0.1:5000/images/';
+
+    const [posts, setPosts] = useState([]);
+    const { search } = useLocation();
+
+    // Fetch posts on component mount
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                // Fetch posts from your API or data source
+                const response = await axios.get('/posts');
+                setPosts(response.data); // Assuming response.data contains posts
+                console.log(response.data);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchPosts();
+    }, [search]);
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const activateInput = () => {
+        setIsInputActive(true);
+    };
+
+    const deactivateInput = () => {
+        setIsInputActive(false);
+    };
+
+    const handleSearch = () => {
+        const matchingPosts = posts.filter(
+            (post) => post.title && post.title.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
+
+        if (matchingPosts.length > 0) {
+            const postId = matchingPosts[0]._id; // Considering only the first match for simplicity
+            window.location.href = `http://127.0.0.1:3000/post/${postId}`;
+        } else {
+            <ToastMessage />
+            console.log('No results found');
+        }
+    };
 
     const goToPageOne = () => {
         if (location.pathname !== 'http://127.0.0.1:3000/') {
-            window.location.href = 'http://127.0.0.1:3000/'
+            window.location.href = 'http://127.0.0.1:3000/';
         }
-    }
+    };
 
     const handleLogout = () => {
         dispatch({ type: 'LOGOUT' });
@@ -66,7 +115,29 @@ function Topbar() {
                         </li>
                     </ul>
                 )}
-                <FontAwesomeIcon className={cx('search-icon')} icon={faSearch} />
+                <div className={cx('button-wrapper')}>
+                    {isInputActive ? (
+                        <div>
+                            <input
+                                className={cx('search-input')}
+                                type="text"
+                                value={searchTerm}
+                                onChange={handleInputChange}
+                                placeholder="Search posts..."
+                            />
+                            <button className={cx('button')} onClick={handleSearch}>
+                                <FontAwesomeIcon className={cx('icon-inner')} icon={faSearch} />
+                            </button>
+                            <button onClick={deactivateInput} className={cx('button')}>
+                                <FontAwesomeIcon className={cx('icon-inner')} icon={faXmark} />
+                            </button>
+                        </div>
+                    ) : (
+                        <button onClick={activateInput} className={cx('button')}>
+                            <FontAwesomeIcon className={cx('icon')} icon={faSearch} />
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
